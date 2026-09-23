d9b4c7a23ecdcfb0790a87ea4052be3384eb1421
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-24 08:30:08 +0900

    feat: create train area

--- train.ts
+++ train.ts
@@ -2,6 +2,7 @@
 
 // Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
 // MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
+
 function reverseSentence(text) {
   const word = text.split(" ");
   const chappaWord = word.map((word) => word.split("").reverse().join(""));
