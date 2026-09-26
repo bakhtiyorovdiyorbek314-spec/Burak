b9a830c978e76e05472c04e8090350ed96a0b6d5
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-26 11:39:56 +0900

    feat: create routing and member controller

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,3 @@
-// console.log("EXECUTED");
-
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-// const person: string = "Deen";
-// const count: number = 100;
-
-// // Architectural Pattern : MVC, Dependency Injection, MVP
-
-// //Design Pattern:Middleware, Decotar
-
 import dotenv from "dotenv";
 dotenv.config();
 
--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,19 @@
+// TASK N:
+
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+
+// MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+
+function getString(string: string) {
+  const string1 = string.split("").reverse().join("");
+
+  if (string === string1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(getString("level"));
 // TASK M:
 
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
