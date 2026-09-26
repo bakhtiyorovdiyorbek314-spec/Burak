94521210a8f9c9753d3cc58530ca744fb890acac
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-26 12:05:40 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./router.Admin";
 
 /**1- ENTERANSE **/
 const app = express();
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**4- ROUTERS **/
-app.use("/", router); //Middlware DP
+app.use("/admin", routerAdmin); //SSR: EJS
+app.use("/", router); //SPA : REACT
 
 export default app;
