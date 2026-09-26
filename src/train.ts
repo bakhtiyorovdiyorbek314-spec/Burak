ff54574a39e27e65555ba823966755de3dd2e3aa
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-27 01:07:15 +0900

    feat: TASK-O is done

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,48 @@
+// TASK O:
+
+// Shunday function yozing va u har xil qiymatlardan iborat array qabul qilsin.
+// Va array ichidagi sonlar yig'indisini hisoblab chiqgan javobni qaytarsin
+
+// MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]); return 45
+
+// Yuqoridagi misolda array tarkibida faqatgina ikkita yagona son mavjud bular 10 hamda 35
+// Qolganlari nested bo'lib yoki type'lari number emas.
+
+function calculateSumOfNumbers(array: any[]): number {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      const a: string = (sum += array[i]);
+    }
+  }
+  return sum;
+}
+
+console.log(calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]));
+
+// function calculateSumOfNumbers(array: unknown[]): number {
+//   return array.reduce<number>((total, value) => {
+//     return typeof value === "number" ? total + value : total;
+//   }, 0);
+// }
+// console.log(calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]));
+
 // TASK N:
 
 // Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
 
 // MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
 
-function getString(string: string) {
-  const string1 = string.split("").reverse().join("");
+// function getString(string: string) {
+//   const string1 = string.split("").reverse().join("");
 
-  if (string === string1) {
-    return true;
-  } else {
-    return false;
-  }
-}
-console.log(getString("level"));
+//   if (string === string1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(getString("level"));
 // TASK M:
 
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
